91b71acb72052534408822246ce3ca515b21ce2b
Lindon Ng <lindonng@hotmail.com>
2019-08-19 12:13:23 +0800

    Implemented Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
+
         printMessage("Hello I'm Duke\nWhat can I do for you?");
 
         String input;
-
-        while(!(input=inputScanner.next()).equals("bye")){
-            printMessage((input));
+        //should use nextLine and not next, else sentences will be split by spaces.
+        while(!(input=inputScanner.nextLine()).equals("bye")){
+            if (input.equals("list")) {
+                printList(taskList);
+            } else {
+                taskList.add(input);
+                printMessage("added: " + input);
+            }
         }
 
         printMessage("Bye. Hope to see you again soon!");
@@ -17,5 +25,16 @@ public class Duke {
 
     public static void printMessage(String msg) {
         System.out.println(msg);
+    }
+
+    public static void printList(ArrayList<String> taskList) {
+        int start = 1;
+        String outputString = "";
+
+        for (String value : taskList) {
+            outputString += start + ". " + value + "\n";
+            start++;
+        }
+        printMessage(outputString);
     }
 }
