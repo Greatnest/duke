655311ce0b8123ab4f65bf7ae2bdc8758ecf83ec
Lindon Ng <lindonng@hotmail.com>
2019-08-19 12:30:25 +0800

    Implemented Level 3. Mark as Done with Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,37 +4,61 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
-        printMessage("Hello I'm Duke\nWhat can I do for you?");
+        printMessage("Hello! I'm Duke\nWhat can I do for you?");
 
         String input;
         //should use nextLine and not next, else sentences will be split by spaces.
         while(!(input=inputScanner.nextLine()).equals("bye")){
-            if (input.equals("list")) {
+            if (input.startsWith("done ")) {
+                markTaskAsDone(input, taskList);
+            } else if (input.equals("list")) {
                 printList(taskList);
             } else {
-                taskList.add(input);
+                taskList.add(new Task(input));
                 printMessage("added: " + input);
             }
         }
 
         printMessage("Bye. Hope to see you again soon!");
-
     }
 
     public static void printMessage(String msg) {
         System.out.println(msg);
     }
 
-    public static void printList(ArrayList<String> taskList) {
+    public static void printList(ArrayList<Task> taskList) {
         int start = 1;
         String outputString = "";
+        outputString += "Here are the tasks in your list:\n";
 
-        for (String value : taskList) {
-            outputString += start + ". " + value + "\n";
-            start++;
+        for (Task value : taskList) {
+            if (start == taskList.size()) {
+                outputString += start + ".[" + value.getStatusIcon() + "] " + value.getDescription();
+            } else {
+                outputString += start + ".[" + value.getStatusIcon() + "] " + value.getDescription() + '\n';
+                start++;
+            }
         }
         printMessage(outputString);
+    }
+
+    public static void markTaskAsDone(String input, ArrayList<Task> taskList) {
+        int taskNumber;
+        try {
+            taskNumber = Integer.parseInt(input.substring(5));
+        } catch (NumberFormatException e) {
+            printMessage("Please enter a number.");
+            return;
+        }
+
+        Task item = taskList.get(taskNumber-1);
+        if (item.getIsDone()) {
+            printMessage("Task is already done.");
+            return;
+        }
+        item.markAsDone();
+        printMessage("Nice! I've marked this task as done: \n  [" + item.getStatusIcon() + "] " + item.getDescription());
     }
 }
